f30c8546d41ac976b65be84751b6888a55958193
himansh-dev <himanshmewada1@gmail.com>
2025-12-24 22:15:56 +0530

    Started Chapter 5

--- 5-1.c
+++ 5-1.c
@@ -1,5 +1,5 @@
-#include <stdio.h>
 #include <ctype.h>
+#include <stdio.h>
 
 #define SIZE 100
 #define BUFSIZE 100
@@ -11,61 +11,52 @@ int getch(void);
 void ungetch(int);
 int getint(int *);
 
-int main()
-{
-    int n, array[SIZE];
+int main() {
+  int n, array[SIZE];
 
-    for (n = 0; n < SIZE; n++)
-        array[n] = 0;
+  for (n = 0; n < SIZE; n++)
+    array[n] = 0;
 
-    for (n = 0; n < SIZE && getint(&array[n]) != EOF; n++)
-        ;
+  for (n = 0; n < SIZE && getint(&array[n]) != EOF; n++)
+    ;
 
-    for (n = 0; n < SIZE; n++)
-    {
-        printf("%d\n", array[n]);
-    }
+  for (n = 0; n < SIZE; n++) {
+    printf("%d\n", array[n]);
+  }
+  return 0;
+}
+
+int getint(int *pn) {
+  int c, sign;
+
+  while (isspace(c = getch()))
+    ;
+  if (!isdigit(c) && c != EOF && c != '+' && c != '-') {
+    ungetch(c);
     return 0;
-}
-
-int getint(int *pn)
-{
-    int c, sign;
-
-    while(isspace(c = getch()))
-        ;
-    if(!isdigit(c) && c != EOF && c != '+' && c != '-') {
-        ungetch(c);
-        return 0;
+  }
+  sign = (c == '-') ? -1 : 1;
+  if (c == '+' || c == '-') {
+    c = getch();
+    if (!isdigit(c)) {
+      ungetch(c);
+      return 0;
     }
-    sign = (c == '-') ? -1 : 1;
-    if (c == '+' || c == '-')
-    {
-        c = getch();
-        if (!isdigit(c))
-        {
-            ungetch(c);
-            return 0;
-        }
-    }
-    for (*pn = 0; isdigit(c); c = getch())
-        *pn = 10 * *pn + (c - '0');
-    *pn *= sign;
+  }
+  for (*pn = 0; isdigit(c); c = getch())
+    *pn = 10 * *pn + (c - '0');
+  *pn *= sign;
 
-    if (c != EOF)
-        ungetch(c);
-    return c;
+  if (c != EOF)
+    ungetch(c);
+  return c;
 }
 
-int getch(void)
-{
-    return (bufp > 0) ? buf[--bufp] : getchar();
-}
+int getch(void) { return (bufp > 0) ? buf[--bufp] : getchar(); }
 
-void ungetch(int c)
-{
-    if (bufp >= BUFSIZE)
-        printf("ungetch: too many characters\n");
-    else
-        buf[bufp++] = c;
+void ungetch(int c) {
+  if (bufp >= BUFSIZE)
+    printf("ungetch: too many characters\n");
+  else
+    buf[bufp++] = c;
 }
