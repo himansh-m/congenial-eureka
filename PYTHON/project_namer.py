58323bf0eebde8f72d108bbf33505f3df74665ea
himansh-dev <himanshmewada1@gmail.com>
2025-12-25 22:04:18 +0530

    a helpful script

--- PYTHON/project_namer.py
+++ PYTHON/project_namer.py
@@ -1,6 +1,12 @@
+# Reason for creating this project don't spend the amount of time you can waste on coding.
+# Just run this script and it creates a file with a random and easy name.
+
+
 import namer
 
 project_name = namer.generate()
 
 with open(f"{project_name}.py", "w") as f:
     f.write("""print("Hello, World!")""")
+
+# USE BLACK FOR FORMATTING
